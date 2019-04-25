4251dcefd84d2cc6c9be387bd2eecd7ffe47c00c
SergiMC <sergimuca59@gmail.com>
2019-04-25 09:02:45 +0200

    Update ip-basic-01.sh

--- ip-basic-01.sh
+++ ip-basic-01.sh
@@ -22,7 +22,7 @@ iptables -A INPUT  -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 # obrir la nostra ip
-iptables -A INPUT -s 192.168.0.18 -j ACCEPT
+iptables -A INPUT -s 192.168.2 -j ACCEPT
 iptables -A OUTPUT -d 192.168.0.18 -j ACCEPT
 
 # Xapat ports 80... ############################
@@ -30,15 +30,15 @@ iptables -A OUTPUT -d 192.168.0.18 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 # port 2080 tancat a tothom: reject
 iptables -A INPUT -p tcp --dport 2080 -j REJECT
-# port 2080 tancat a tothom: drop
+# port 3080 tancat a tothom: drop
 iptables -A INPUT -p tcp --dport 3080 -j DROP
-# port 3080 tancat a tothom exceprte i26
+# port 4080 tancat a tothom exceprte i26
 iptables -A INPUT -p tcp --dport 4080 -s 192.168.2.56  -j ACCEPT
 iptables -A INPUT -p tcp --dport 4080 -j DROP
-# port 4080 obert a tothom tancat a i26
+# port 5080 obert a tothom tancat a i26
 iptables -A INPUT -p tcp --dport 5080 -s 192.168.2.56 -j REJECT
 iptables -A INPUT -p tcp --dport 5080 -j ACCEPT
-# port 5080 tancat a tohom, obert a hisx2 tancat a i26
+# port 6080 tancat a tohom, obert a hisx2 tancat a i26
 iptables -A INPUT -p tcp --dport 6080 -s 192.168.2.56 -j REJECT
 iptables -A INPUT -p tcp --dport 6080 -s 192.168.2.0/24 -j ACCEPT
 iptables -A INPUT -p tcp --dport 6080 -j DROP
